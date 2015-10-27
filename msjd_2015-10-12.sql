173174a408a93ca1d808cb454ade7108c726f58e
RhezaBoge <rheza.cloud@gmail.com>
2015-10-27 10:49:43 +0700

    newdatabase

--- msjd_2015-10-12.sql
+++ msjd_2015-10-12.sql
@@ -1,53 +1,51 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.5.42)
-# Database: msjd_2015-10-12
-# Generation Time: 2015-10-21 04:01:49 +0000
-# ************************************************************
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 27, 2015 at 04:48 AM
+-- Server version: 5.5.25a
+-- PHP Version: 5.6.14
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Database: `msjd_2015-10-12`
+--
 
-# Dump of table migrations
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `migrations`;
+--
+-- Table structure for table `migrations`
+--
 
-CREATE TABLE `migrations` (
+CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+--
+-- Dumping data for table `migrations`
+--
 
-INSERT INTO `migrations` (`migration`, `batch`)
-VALUES
-	('2014_10_12_000000_create_users_table',1),
-	('2014_10_12_100000_create_password_resets_table',1);
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1);
 
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `password_resets`
+--
 
-# Dump of table password_resets
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `password_resets`;
-
-CREATE TABLE `password_resets` (
+CREATE TABLE IF NOT EXISTS `password_resets` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -55,39 +53,45 @@ CREATE TABLE `password_resets` (
   KEY `password_resets_token_index` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-LOCK TABLES `password_resets` WRITE;
-/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+--
+-- Dumping data for table `password_resets`
+--
 
-INSERT INTO `password_resets` (`email`, `token`, `created_at`)
-VALUES
-	('rheza.boge@gmail.com','e87360f738bd6d13d92eee0dd983d8d963fdcf83c4a50d455ad82418ad9ed5fe','2015-10-15 15:34:07');
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('rheza.boge@gmail.com', 'e87360f738bd6d13d92eee0dd983d8d963fdcf83c4a50d455ad82418ad9ed5fe', '2015-10-15 08:34:07'),
+('admin@admin.com', 'a8d190f51efbb377ef357d04ac6cfc09fdb5024e05cc6b4317edcd3818be7b83', '2015-10-21 02:32:01');
 
-/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `table_banned_report`
+--
 
-# Dump of table table_banned_report
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `table_banned_report`;
-
-CREATE TABLE `table_banned_report` (
+CREATE TABLE IF NOT EXISTS `table_banned_report` (
   `banned_report_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `users_by` int(11) NOT NULL,
   `content_id` int(11) NOT NULL,
   `users_dest` int(11) NOT NULL,
   `banned_report_message` text NOT NULL,
   PRIMARY KEY (`banned_report_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
+--
+-- Dumping data for table `table_banned_report`
+--
 
+INSERT INTO `table_banned_report` (`banned_report_id`, `users_by`, `content_id`, `users_dest`, `banned_report_message`) VALUES
+(1, 26, 2, 0, '3'),
+(2, 1, 2, 4, '3'),
+(10, 27, 1, 26, '');
 
-# Dump of table table_content
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_content`;
+--
+-- Table structure for table `table_content`
+--
 
-CREATE TABLE `table_content` (
+CREATE TABLE IF NOT EXISTS `table_content` (
   `content_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `content_title` text NOT NULL,
   `content_headline` text NOT NULL,
@@ -100,49 +104,89 @@ CREATE TABLE `table_content` (
   `content_date_expired` datetime NOT NULL,
   `content_publish` varchar(50) NOT NULL DEFAULT '',
   `content_category_id` int(11) NOT NULL,
+  `hashtag_id` text NOT NULL,
   `content_repost_from` int(11) NOT NULL,
   PRIMARY KEY (`content_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `table_content`
+--
 
+INSERT INTO `table_content` (`content_id`, `content_title`, `content_headline`, `content_detail`, `content_media_id`, `content_users_uploader`, `content_last_editor`, `content_date_insert`, `content_date_update`, `content_date_expired`, `content_publish`, `content_category_id`, `hashtag_id`, `content_repost_from`) VALUES
+(1, 'Content 1', '', '', '', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 1, '', 0);
 
-# Dump of table table_content_category
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_content_category`;
+--
+-- Table structure for table `table_content_category`
+--
 
-CREATE TABLE `table_content_category` (
+CREATE TABLE IF NOT EXISTS `table_content_category` (
   `content_category_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `content_category_title` varchar(150) NOT NULL DEFAULT '',
   `content_category_description` text NOT NULL,
   `media_manager_id` int(11) NOT NULL,
   PRIMARY KEY (`content_category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `table_content_category`
+--
 
+INSERT INTO `table_content_category` (`content_category_id`, `content_category_title`, `content_category_description`, `media_manager_id`) VALUES
+(1, 'Content Category 1', 'Deskripsi content category', 1);
 
-# Dump of table table_media_manager
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_media_manager`;
+--
+-- Table structure for table `table_hashtag`
+--
 
-CREATE TABLE `table_media_manager` (
+CREATE TABLE IF NOT EXISTS `table_hashtag` (
+  `hashtag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `hashtag_title` char(255) NOT NULL,
+  PRIMARY KEY (`hashtag_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `table_hashtag`
+--
+
+INSERT INTO `table_hashtag` (`hashtag_id`, `hashtag_title`) VALUES
+(2, '#contoh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_media_manager`
+--
+
+CREATE TABLE IF NOT EXISTS `table_media_manager` (
   `media_manager_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_manager_title` varchar(200) NOT NULL DEFAULT '',
   `media_manager_type` varchar(10) NOT NULL DEFAULT '',
   `media_manager_filename` text NOT NULL,
   `media_manager_publish` char(10) NOT NULL DEFAULT '',
   PRIMARY KEY (`media_manager_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
+--
+-- Dumping data for table `table_media_manager`
+--
 
+INSERT INTO `table_media_manager` (`media_manager_id`, `media_manager_title`, `media_manager_type`, `media_manager_filename`, `media_manager_publish`) VALUES
+(1, 'Media Manager 1', '', '', '1'),
+(2, 'Media Manager 2', '', '', '1'),
+(4, 'Media manager 3', 'blabla', 'blabla', '1');
 
-# Dump of table table_schedule
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_schedule`;
+--
+-- Table structure for table `table_schedule`
+--
 
-CREATE TABLE `table_schedule` (
+CREATE TABLE IF NOT EXISTS `table_schedule` (
   `schedule_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `schedule_title` text NOT NULL,
   `schedule_type_id` int(11) NOT NULL,
@@ -155,110 +199,124 @@ CREATE TABLE `table_schedule` (
   `schedule_media_id` text NOT NULL,
   `schedule_publish` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`schedule_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `table_schedule`
+--
 
+INSERT INTO `table_schedule` (`schedule_id`, `schedule_title`, `schedule_type_id`, `schedule_users_creator`, `schedule_users_source`, `schedule_date_start`, `schedule_date_end`, `schedule_description`, `schedule_headline`, `schedule_media_id`, `schedule_publish`) VALUES
+(1, 'Testing schedule', 1, 26, 26, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'blablabla', 'blablabla', '1,2,3', '0');
 
-# Dump of table table_schedule_type
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_schedule_type`;
+--
+-- Table structure for table `table_schedule_type`
+--
 
-CREATE TABLE `table_schedule_type` (
+CREATE TABLE IF NOT EXISTS `table_schedule_type` (
   `schedule_type_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `schedule_type_name` varchar(200) NOT NULL DEFAULT '',
   `schedule_type_desc` text NOT NULL,
   `media_manager_id` text NOT NULL,
   PRIMARY KEY (`schedule_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `table_schedule_type`
+--
 
+INSERT INTO `table_schedule_type` (`schedule_type_id`, `schedule_type_name`, `schedule_type_desc`, `media_manager_id`) VALUES
+(1, 'Schedule type 1', '', '');
 
-# Dump of table table_users
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_users`;
+--
+-- Table structure for table `table_users_detail`
+--
 
-CREATE TABLE `table_users` (
-  `users_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `table_users_detail` (
+  `users_detail_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
   `users_name` varchar(200) NOT NULL DEFAULT '',
   `users_fullname` varchar(50) NOT NULL DEFAULT '',
   `users_password` text NOT NULL,
   `users_group_id` int(11) NOT NULL,
   `users_email` tinytext NOT NULL,
+  `users_telp` char(13) NOT NULL,
   `users_json_following` text NOT NULL,
   `users_description` text NOT NULL,
   `media_manager_id` text NOT NULL,
   `users_avatar` int(11) NOT NULL,
   `users_status_id` int(11) NOT NULL,
   `users_password_recovery` text NOT NULL,
-  `users_phone` varchar(50) NOT NULL,
-  PRIMARY KEY (`users_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`users_detail_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+--
+-- Dumping data for table `table_users_detail`
+--
 
+INSERT INTO `table_users_detail` (`users_detail_id`, `users_id`, `users_name`, `users_fullname`, `users_password`, `users_group_id`, `users_email`, `users_telp`, `users_json_following`, `users_description`, `media_manager_id`, `users_avatar`, `users_status_id`, `users_password_recovery`) VALUES
+(2, 26, 'ubay', 'bayu', '', 6, 'sdfsfddffsfata@data.com', '', '', '', '', 0, 0, ''),
+(3, 27, 'ubayu', 'bayu', '', 6, 'data@data.com', '', '26,25,', '', '', 0, 0, '');
 
-# Dump of table table_users_group
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `table_users_group`;
+--
+-- Table structure for table `table_users_group`
+--
 
-CREATE TABLE `table_users_group` (
+CREATE TABLE IF NOT EXISTS `table_users_group` (
   `users_group_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `users_group_name` varchar(100) NOT NULL DEFAULT '',
   `users_group_description` text NOT NULL,
   `users_group_is_public` int(11) NOT NULL,
   PRIMARY KEY (`users_group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
-LOCK TABLES `table_users_group` WRITE;
-/*!40000 ALTER TABLE `table_users_group` DISABLE KEYS */;
+--
+-- Dumping data for table `table_users_group`
+--
 
-INSERT INTO `table_users_group` (`users_group_id`, `users_group_name`, `users_group_description`, `users_group_is_public`)
-VALUES
-	(1,'Superadmin','',0),
-	(2,'Administrator','',0),
-	(3,'Content Writer','',0),
-	(4,'Jamaah','',0),
-	(5,'Ustadz','',0),
-	(6,'Masjid','Masjid',1);
+INSERT INTO `table_users_group` (`users_group_id`, `users_group_name`, `users_group_description`, `users_group_is_public`) VALUES
+(1, 'Superadmin', '', 0),
+(2, 'Administrator', '', 0),
+(3, 'Content Writer', '', 0),
+(4, 'Jamaah', '', 0),
+(5, 'Ustadz', '', 0),
+(6, 'Masjid', 'Masjid', 1);
 
-/*!40000 ALTER TABLE `table_users_group` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `table_users_status`
+--
 
-# Dump of table table_users_status
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `table_users_status`;
-
-CREATE TABLE `table_users_status` (
+CREATE TABLE IF NOT EXISTS `table_users_status` (
   `users_status_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `users_status_title` varchar(100) NOT NULL DEFAULT '',
   `users_status_desc` text NOT NULL,
   PRIMARY KEY (`users_status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
-LOCK TABLES `table_users_status` WRITE;
-/*!40000 ALTER TABLE `table_users_status` DISABLE KEYS */;
+--
+-- Dumping data for table `table_users_status`
+--
 
-INSERT INTO `table_users_status` (`users_status_id`, `users_status_title`, `users_status_desc`)
-VALUES
-	(1,'Pending','pending users'),
-	(2,'Active',''),
-	(3,'Banned',''),
-	(4,'Reported','');
+INSERT INTO `table_users_status` (`users_status_id`, `users_status_title`, `users_status_desc`) VALUES
+(1, 'Pending', 'pending users'),
+(2, 'Active', ''),
+(3, 'Banned', ''),
+(4, 'Reported', '');
 
-/*!40000 ALTER TABLE `table_users_status` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `users`
+--
 
-# Dump of table users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -268,25 +326,16 @@ CREATE TABLE `users` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=28 ;
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+--
+-- Dumping data for table `users`
+--
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`)
-VALUES
-	(1,'Rheza','rheza.boge@gmail.com','$2y$10$vGj70E.njYVm1Zm8lu10g.0cT9Vz9ag1XY9CyxpJN.uklOr4rKZ82','vgW5VWnmj73dpLcrgBkhaqUBXHOslwiAhUFw5rI9eOXi2saytMmSUVbmH05X','2015-10-15 15:30:14','2015-10-15 17:25:36'),
-	(2,'Rheza2','rheza2.boge@gmail.com','$2y$10$jE84xQoX/HjmXa5lCOKb5OTMOXoINK/QcBanjXF6NYc9bOPnend4i','n7aXJrTRqpeLeQFnRtyyi4lHTLslUXgSfvyxPZs0wJeoQt9U8A1gmTxqlMPl','2015-10-15 16:37:38','2015-10-15 16:44:29'),
-	(3,'budiartoy','eko@hotmail.com','8c7d5676fb7a672cd7d9a2e566f54bd9',NULL,'2015-10-20 14:40:12','2015-10-20 14:40:12');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Rheza2', 'admin@admin.com', '$2y$10$jE84xQoX/HjmXa5lCOKb5OTMOXoINK/QcBanjXF6NYc9bOPnend4i', 'n7aXJrTRqpeLeQFnRtyyi4lHTLslUXgSfvyxPZs0wJeoQt9U8A1gmTxqlMPl', '2015-10-15 09:37:38', '2015-10-15 09:44:29'),
+(27, 'ubay', 'data@data.com', '$2y$10$nipTEi5yPMPtJiWBdHxidONj6CItXq2KyGhNwA7ihisYqM6vNqiL6', NULL, '2015-10-21 06:49:57', '2015-10-21 06:49:57');
 
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
